ee3ee6f85fed4271999458db212ca301e617c28e
CetinaLuka <luka.cetina@student.um.si>
2021-03-08 13:19:19 +0100

    feat: added project

---
+++ index.js
@@ -0,0 +1,51 @@
+const { createServer } = require('http');
+const fs = require('fs')
+const PORT = 3000;
+
+const server = createServer();
+server.on('request', (req, res) => { 
+
+    var url = req.url;
+    console.log(url);
+    if(url === "/"){
+        res.writeHead(200, { 'Content-type': 'text/html; charset=utf-8'});
+        fs.readFile('assets/html/links.html', function(err, data) {
+            return res.end(data);
+        });
+    }
+    else if(url === "/funkcionalnosti-streznika"){
+        res.writeHead(200, { 'Content-type': 'text/html; charset=utf-8'});
+        fs.readFile('assets/html/index.html', function(err, data) {
+
+            return res.end(data);
+        });
+    }
+    else if(url === "/posebnosti"){
+        res.writeHead(200, { 'Content-type': 'text/html; charset=utf-8'});
+        fs.readFile('assets/text/posebnosti.txt', function(err, data) {
+
+            return res.end(data);
+        });
+    }
+    else if(url === "/style.css"){
+        res.writeHead(200, { 'Content-type': 'text/html; charset=utf-8'});
+        fs.readFile('assets/css/style.css', function(err, data) {
+            return res.end(data);
+        });
+    }
+    else if(url === "/use-case-diagram"){
+        res.writeHead(200, { 'Content-type': 'image/gif'});
+        fs.readFile('assets/img/uml.jpg', function(err, data) {
+            return res.end(data);
+        });
+    }
+    else if(url.match('\/img\/')){
+        var filename = url.substr(5);
+        res.writeHead(200, { 'Content-type': 'image/gif'});
+        fs.readFile('assets/img/'+filename, function(err, data) {
+            return res.end(data);
+        });
+    }
+}); 
+
+server.listen(PORT, () => console.log("Server listening on port " + PORT));
